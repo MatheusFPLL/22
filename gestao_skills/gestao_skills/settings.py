dd63d0a7dfee0fa57a12b03d6e04c8856d63eadd
MatheusFPLL <matheusflich@hotmail.com>
2024-09-10 21:40:56 -0300

    ultima versap

--- gestao_skills/gestao_skills/settings.py
+++ gestao_skills/gestao_skills/settings.py
@@ -37,6 +37,7 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'skills'
 ]
 
 MIDDLEWARE = [
@@ -54,7 +55,7 @@ ROOT_URLCONF = 'gestao_skills.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [],
+        'DIRS': [BASE_DIR / 'skills' / 'templates'],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
